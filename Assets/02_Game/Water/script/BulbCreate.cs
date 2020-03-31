8cdcd4e6dbcd403cdefc6cae62247338c1e5a714
Shun Kato <junk-machine0524@live.jp>
2020-03-31 20:29:28 +0900

    水演出つき箱の追加

---
+++ Assets/02_Game/Water/script/BulbCreate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulbCreate : MonoBehaviour
+{
+    private BlockTank script;
+    private bool bFirst;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bFirst = false;
+        script = this.GetComponent<BlockTank>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(script._fullWater)
+        {
+            if(!bFirst)
+            {
+                bFirst = true;
+                GameObject obj = (GameObject)Resources.Load("WaterBulb_in");
+
+                Vector3 pos = this.transform.position;
+                pos.y -= 0.4f;
+
+
+                Instantiate(obj, pos, Quaternion.identity);
+            }
+        }
+    }
+}
