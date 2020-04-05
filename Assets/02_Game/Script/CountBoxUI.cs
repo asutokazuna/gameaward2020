8dec23a620fa8d5c6e436f90fda87b1eee521c7e
itorisa <chrysanthemum.zero211@gmail.com>
2020-04-05 21:19:27 +0900

    タグNumber・Boxの追加 箱の数を数えるためのスクリプト追加

---
+++ Assets/02_Game/Script/CountBoxUI.cs
@@ -0,0 +1,51 @@
+﻿/**
+ * @file    CountBoxUI.cs
+ * @brief   箱の全体数や水の満たされた箱の個数を表示するUI管理
+ * @author  Risa Ito
+ * @date    2020/03/30(月)  作成
+ * @date    2020/04/05(日)  画像表示に対応
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * @class CountBoxUI
+ * @brief 箱の全体数や水の満たされた箱の個数を表示するUI管理
+ */
+public class CountBoxUI : MonoBehaviour
+{
+    //! 箱の全体数
+    int _countTatalBox;
+
+    //! タグ指定用
+    public string _countBoxTag;
+
+    // 画像表示用
+    public NumberImage _tatalBoxImage;      //!< 箱全体数画像表示用
+    public FullBoxImage _fullBoxImage;      //!< 箱全体数画像表示用
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 指定されたタグ付きオブジェクトの個数を数える
+        GameObject[] _boxObjects = GameObject.FindGameObjectsWithTag(_countBoxTag);
+        _countTatalBox = _boxObjects.Length;
+
+        // 画像表示
+        _tatalBoxImage.AddValue(_countTatalBox);
+    }
+
+    /**
+    * @brief        満たされた箱の数の追加
+    * @param[in]    int 満たされた箱の数の増減量
+    * @return       なし
+    * @details      満たされた箱の数の増減をセットする関数です
+    */
+    public void AddFullBox(int fullBoxNum)
+    {
+        _fullBoxImage.AddFullBox(fullBoxNum);
+    }
+}
