0798fbe90d875ea9663f46ed5fb283b0b25718d5
Shun Kato <junk-machine0524@live.jp>
2020-04-06 18:40:37 +0900

    フラグの修正、およびパーティクル化

--- Assets/02_Game/Water/script/BulbCreate.cs
+++ Assets/02_Game/Water/script/BulbCreate.cs
@@ -7,6 +7,8 @@ public class BulbCreate : MonoBehaviour
     private BlockTank script;
     private bool bFirst;
 
+    private int nOldWater;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +19,28 @@ public class BulbCreate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(script._fullWater)
+        if(script._numWater == 0)
+        {
+            bFirst = false;
+
+            if(nOldWater > 0)
+            {
+                GameObject obj = (GameObject)Resources.Load("WaterBulb_out");
+
+                Vector3 pos = this.transform.position;
+                pos.y += 0.4f;
+
+
+                Instantiate(obj, pos, Quaternion.identity);
+            }
+        }
+
+        if (script._numWater > 0)
         {
             if(!bFirst)
             {
                 bFirst = true;
+
                 GameObject obj = (GameObject)Resources.Load("WaterBulb_in");
 
                 Vector3 pos = this.transform.position;
@@ -31,5 +50,8 @@ public class BulbCreate : MonoBehaviour
                 Instantiate(obj, pos, Quaternion.identity);
             }
         }
+
+
+        nOldWater = script._numWater;
     }
 }
--- Assets/02_Game/Water/script/BulbDestroy.cs
+++ Assets/02_Game/Water/script/BulbDestroy.cs
@@ -1,28 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NVIDIA.Flex;
+
 
 public class BulbDestroy : MonoBehaviour
 {
     public float fTargetTime;//消滅までの時間
     private float fTime;//経過時間
-
+    private bool bFlg;
 
     // Start is called before the first frame update
     void Start()
     {
         fTime = fTargetTime;
+        bFlg = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+       
         fTime -= Time.deltaTime;
 
         if(fTime <= 0.0f)
         {
+            // this.gameObject.GetComponent<NVIDIA.Flex.FlexSourceActor>().startSpeed = 0.5f;
             //this.gameObject.GetComponent<NVIDIA.Flex.FlexSourceActor>().isActive = false;
             Destroy(gameObject);
+            
+            bFlg = !bFlg;
+            fTime = fTargetTime;
+        }
+
+        if (bFlg)
+        {
+            //this.gameObject.GetComponent<NVIDIA.Flex.FlexFluidRenderer>().enabled = false;
+        }
+        else
+        {
+           // this.gameObject.GetComponent<NVIDIA.Flex.FlexFluidRenderer>().enabled = true;
         }
     }
 }
