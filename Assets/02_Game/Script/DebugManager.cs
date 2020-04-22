635c274d8ba4deaf32b315f633cd64f806069772
Shun Kato <junk-machine0524@live.jp>
2020-04-22 15:16:20 +0900

    クリア演出の追加、マネージャー系をわかりやすく再配置

---
+++ Assets/02_Game/Script/DebugManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugManager : MonoBehaviour
+{
+    public bool _isDebug;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/02_Game/Script/manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool _isDebug;
+        _isDebug = GameObject.Find("DebugManager").GetComponent<DebugManager>()._isDebug;
+        if(_isDebug)
+        {
+            this.GetComponent<MeshRenderer>().enabled = true;
+        }
+        else
+        {
+            this.GetComponent<MeshRenderer>().enabled = false;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
