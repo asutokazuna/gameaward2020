4da18fbdfc71809428b1a5f8b6a24772390633d1
kon_blanc <konblanc@gmail.com>
2020-04-22 15:24:37 +0900

    水のテクスチャの色をパーティクルと同じになるようにした 箱に水のテクスチャを追加 要らん箱を削除

--- Assets/02_Game/Water/Water_Texture/meshUV.cs
+++ Assets/02_Game/Water/Water_Texture/meshUV.cs
@@ -7,8 +7,8 @@ public class meshUV : MonoBehaviour
     private Mesh _myMesh;       //UV操作用
     private MeshRenderer _mr;       //カラー操作用
     private GameObject _parent;     //親情報取得用
-    private BlockTank _myScript;        //コンポーネント取得用
-    private Color _color;        //色保存用
+    private WaterFlow _myScript;        //コンポーネント取得用
+    [SerializeField] private Color _color;        //色保存用
 
     // Start is called before the first frame update
     void Start()
@@ -16,8 +16,8 @@ public class meshUV : MonoBehaviour
         _myMesh = gameObject.GetComponent<MeshFilter>().mesh;
         _mr = GetComponent<MeshRenderer>();
         _parent = transform.parent.gameObject;
-        _myScript = _parent.GetComponent<BlockTank>();
-        _color = _mr.material.color;
+        _myScript = _parent.GetComponent<WaterFlow>();
+        _color = _parent.GetComponent<ParticleSystem>().colorOverLifetime.color.gradient.colorKeys[0].color;    //パーティクルシステムの色を取得
         _mr.material.color = new Color(_color.r, _color.g, _color.b, 0);
     }
 
@@ -33,11 +33,11 @@ public class meshUV : MonoBehaviour
         }
         _myMesh.uv = nUV;
 
-        if (_myScript._numWater >= _myScript._maxWater && _mr.material.color.a < _color.a)
+        if (_myScript._isFullWater)
         {
-            _mr.material.color += new Color(0, 0, 0, 0.0001f);
+            _mr.material.color = _color;
         }
-        else if (_myScript._numWater <= 0 && _mr.material.color.a >= _color.a)
+        else if (_myScript._currentWater >= 0)
         {
             _mr.material.color = new Color(_color.r, _color.g, _color.b, 0);
         }
