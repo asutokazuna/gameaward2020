e82c72d2d3e6c033c8dd33c4b1774018f3dfd33a
Shun Kato <junk-machine0524@live.jp>
2020-04-24 18:51:32 +0900

    惑星セレクト機能の追加

---
+++ Assets/04_Galaxy/script/CameraMove.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent _agent;
+    public int _currentID;
+    public float _distanceToPlanet;
+    public float _cameraMoveSpd1;
+    public float _cameraMoveSpd2;
+    public float _cameraRotateSpd;
+
+    private GameObject[] _planetData;
+    private GameObject _currentPlanet;
+    private Transform _camera;
+
+    [SerializeField]
+    private bool _isOrbital;
+    [SerializeField]
+    private float _cameraSpd;
+    private Vector3 _angles;
+
+    public float _rotateSpeed;
+    private float _angleX;
+    private float _angleY;
+
+    [SerializeField] private float mouseY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _planetData = GameObject.FindGameObjectsWithTag("Planet");
+        _camera = this.transform.GetComponentInChildren<Transform>();
+        _isOrbital = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            _currentID--;
+            _isOrbital = false;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _currentID++;
+            _isOrbital = false;
+        }
+
+        
+        for(int i = _planetData.Length - 1;i >= 0;i--)
+        {
+            if(_planetData[i].GetComponent<revolution>().PlanetID == _currentID)
+            {
+                _currentPlanet = _planetData[i];
+                break;
+            }
+        }
+        
+        if (!_isOrbital)
+        {
+            _agent.destination = _currentPlanet.transform.position;
+            _cameraSpd = _cameraRotateSpd;
+        }
+        else
+        {
+            _cameraSpd = 1.0f;
+
+            _angleX += Input.GetAxis("Mouse X") * _rotateSpeed;
+            CheckAngle(_angleX);
+
+            _angleY += Input.GetAxis("Mouse Y") * _rotateSpeed;
+            if(_angleY * _rotateSpeed <= 0)
+            {
+                _angleY = 0;
+            }
+            if (_angleY  >= 3)
+            {
+                _angleY = 3;
+            }
+
+            mouseY = _angleY;
+
+            Vector3 _pos;
+            _pos.x = _distanceToPlanet * Mathf.Sin(_angleX) + _currentPlanet.transform.position.x;
+            _pos.y = _distanceToPlanet * Mathf.Cos(_angleY) + _currentPlanet.transform.position.y;
+            _pos.z = _distanceToPlanet * Mathf.Cos(_angleX) + _currentPlanet.transform.position.z;
+
+
+            transform.position = _pos;
+
+            
+        }
+
+        Vector3 _relativePos = _currentPlanet.transform.position - _camera.position;
+        Quaternion _rotation = Quaternion.LookRotation(_relativePos);
+        _camera.rotation = Quaternion.Slerp(_camera.rotation, _rotation, _cameraSpd);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<revolution>().PlanetID == _currentID)
+        {
+            _isOrbital = true;
+            Vector3 _axis = _camera.position - _currentPlanet.transform.position;
+        }
+    }
+
+    private float CheckAngle(float angle)
+    {
+        if(angle < 360.0f)
+        {
+            angle += 360.0f;
+        }
+        if (angle > 360.0f)
+        {
+            angle -= 360.0f;
+        }
+
+        return angle;
+    }
+}
