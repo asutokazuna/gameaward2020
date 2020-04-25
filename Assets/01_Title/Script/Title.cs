80e97823c90bdd11da4ffc019b0665af173400fb
KotaNakagami <nakagamikouta3@gmail.com>
2020-04-26 02:32:43 +0900

    Revert "Merge branch 'feature/Texture' into 'develop'"
    This reverts merge request !143

--- Assets/01_Title/Script/Title.cs
+++ Assets/01_Title/Script/Title.cs
@@ -5,27 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
-    bool _LogoAnimFinish;
-
-    void Start()
-    {
-        _LogoAnimFinish = false;
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if (_LogoAnimFinish)
+        if (Input.anyKey)
         {
-            if (Input.anyKey)
-            {
-                GameObject.FindGameObjectWithTag("SceneManager").GetComponent<SceneMgr>().SetScene(E_SCENE.STAGE_SELECT);
-            }
+            GameObject.FindGameObjectWithTag("SceneManager").GetComponent<SceneMgr>().SetScene(E_SCENE.STAGE_SELECT);
         }
-    }
-
-    void SetLogoAnimFinish()
-    {
-        _LogoAnimFinish = true;
     }
 }
