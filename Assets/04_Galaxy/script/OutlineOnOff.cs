a05f7d7434c3df1e9bb8ee09effb8e907aab2db3
hal_tsuboi_kazuki <hal.tsuboi.kazuki@gmail.com>
2020-04-28 11:09:41 +0900

    取り合えず04_Galaxyへのスクリプトの追加のみ.

---
+++ Assets/04_Galaxy/script/OutlineOnOff.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class OutlineOnOff : MonoBehaviour
+{
+    private MeshRenderer _meshRend;
+    [SerializeField] float _outlineWidth;
+    [SerializeField] Color _outlineColor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _meshRend = GetComponent<MeshRenderer>();
+        _meshRend.material.SetFloat("_Outline_Width", 0.0f);
+        _meshRend.material.SetColor("_Outline_Color", _outlineColor);
+
+        Invoke("OutlineOn", 5.0f);
+        Invoke("OutlineOff", 10.0f);
+
+    }
+
+    public void OutlineOn()
+    {
+        _meshRend.material.SetFloat("_Outline_Width", _outlineWidth);
+        _meshRend.material.SetColor("_Outline_Color", _outlineColor);
+    }
+
+    public void OutlineOff()
+    {
+        _meshRend.material.SetFloat("_Outline_Width", 0.0f);
+    }
+
+    //ColorとWidthを実行後に変更したい時用. ※OutlineOnを呼ぶまでは適用されない.
+    public void OutlineCustom(Color col, float width)
+    {
+        _outlineWidth = width;
+        _outlineColor = col;
+    }
+
+}
