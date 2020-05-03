ac4c214bf2cfcc88345381bbdca0f8332d456772
hal_tsuboi_kazuki <hal.tsuboi.kazuki@gmail.com>
2020-05-03 16:08:50 +0900

    溜まり切った水の別Verを実装。

---
+++ Assets/02_Game/Water/script/WaterWave.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterWave : MonoBehaviour
+{
+    private MeshRenderer _meshRend;
+    private GameObject _parent;
+    private WaterFlow _myScript;        //コンポーネント取得用
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _meshRend = GetComponent<MeshRenderer>();
+        _parent = transform.parent.gameObject;
+        _myScript = _parent.GetComponent<WaterFlow>();
+        _meshRend.material.SetFloat("_Distortion", 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_myScript._currentWater >= _myScript._maxWater - 35)
+        {
+            _meshRend.material.SetFloat("_Distortion", 1);
+        }
+        else if (_myScript._currentWater <= 0)
+        {
+            _meshRend.material.SetFloat("_Distortion", 0);
+        }
+
+    }
+}
