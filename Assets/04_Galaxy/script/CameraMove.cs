a342437a8b759708d3dfe9c2e2464d93837e3da7
Shun Kato <junk-machine0524@live.jp>
2020-05-04 16:39:16 +0900

    カメラワークを調整

--- Assets/04_Galaxy/script/CameraMove.cs
+++ Assets/04_Galaxy/script/CameraMove.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 public class CameraMove : MonoBehaviour
 {
     [SerializeField]
-    private NavMeshAgent _agent;
+    private NavMeshAgent _agent = default;
     public int _currentID;
     public float _distanceToPlanet;
     public float _cameraMoveSpd1;
@@ -17,8 +17,8 @@ public class CameraMove : MonoBehaviour
     private GameObject _currentPlanet;
     private Transform _camera;
 
-    [SerializeField]
-    private bool _isOrbital;
+  
+    public bool _isOrbital;
     [SerializeField]
     private float _cameraSpd;
     private Vector3 _angles;
@@ -34,6 +34,8 @@ public class CameraMove : MonoBehaviour
     private SceneMgr SceneManager;
 
     public float _offsetY;
+
+   
 
     // Start is called before the first frame update
     void Start()
--- Assets/04_Galaxy/script/revolution.cs
+++ Assets/04_Galaxy/script/revolution.cs
@@ -9,6 +9,8 @@ public class revolution : MonoBehaviour
     public float _orbitalSpeed;
     public float _startAngle;
 
+    private CameraMove _script = null;
+
     private float _angle;
     // Start is called before the first frame update
     void Start()
@@ -18,26 +20,36 @@ public class revolution : MonoBehaviour
 
         _angle = _startAngle;
         _orbitalSpeed *= 0.001f;
+
+        _script = GameObject.Find("CameraObj").GetComponent<CameraMove>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = new Vector3(_orbitalDistance * Mathf.Sin(_angle),
-            0.0f,_orbitalDistance * Mathf.Cos(_angle));
-        _angle += _orbitalSpeed;
-
-
-
-
-
-        if(_angle < 360.0f)
+        if(_script._isOrbital)
         {
-            _angle += 360.0f;
+
         }
-        if (_angle > 360.0f)
+        else
         {
-            _angle -= 360.0f;
+            this.transform.position = new Vector3(_orbitalDistance * Mathf.Sin(_angle),
+           0.0f, _orbitalDistance * Mathf.Cos(_angle));
+            _angle += _orbitalSpeed;
+
+
+
+
+
+            if (_angle < 360.0f)
+            {
+                _angle += 360.0f;
+            }
+            if (_angle > 360.0f)
+            {
+                _angle -= 360.0f;
+            }
         }
+       
     }
 }
