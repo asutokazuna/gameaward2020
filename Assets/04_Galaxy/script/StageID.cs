b8b29c3d04202cc062cb142a64cc4f0925ee89b8
kito0709 <kaisei.yanai.871@gmail.com>
2020-05-04 19:16:15 +0900

    no message

---
+++ Assets/04_Galaxy/script/StageID.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageID : MonoBehaviour
+{
+    public E_SCENE _stageID = 0;//!<例外処理シーンほしい
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
