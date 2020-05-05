645f113dcd508a9fb138b140eadd89fe09abd8c8
itorisa <chrysanthemum.zero211@gmail.com>
2020-05-05 19:42:23 +0900

    ステージ番号がアニメーションの途中で変わってしまうのを修正

--- Assets/04_Galaxy/script/StageSelectUI.cs
+++ Assets/04_Galaxy/script/StageSelectUI.cs
@@ -1,7 +1,17 @@
-﻿using System.Collections;
+﻿/**
+ * @file    StageSelectUI.cs
+ * @brief   ステージ番号管理用
+ * @author  Risa Ito
+ * @date    2020/03/30(月)  作成
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * @class StageSelectUI
+ * @brief ステージ番号管理用
+ */
 public class StageSelectUI : MonoBehaviour
 {
     [SerializeField] int _stageNum;     //!< 1惑星当たりのステージ数
@@ -24,36 +34,45 @@ public class StageSelectUI : MonoBehaviour
     {
         // 選択されているIDの取得
         _stageId = GameObject.Find("CameraObj").GetComponent<RaySystem>().GetID();
+
+        // 選択対象が変更されたらUI変更
         if (_stageId != _oldStageId)
         {
             SetStageNo();
         }
     }
 
+    /**
+    * @brief        ステージ番号のセット
+    * @return       なし
+    * @details      ステージ番号の変更をセットする関数です
+    */
     void SetStageNo()
     {
+        // ステージIDをステージ番号に計算
         int stageNo = (int)_stageId - 1;
 
+        // 星の数分ステージ番号を補正
         while(stageNo > _stageNum)
         {
             stageNo -= _stageNum;
         }
 
-        _oldStageId = _stageId;
-
         if (stageNo > 0)
         {
-            _stageSelectImage.SetValue(stageNo);
-            _stageSelectAnim.SetBool("Select", true);
+            // 島が選択された場合
+            if (_stageSelectAnim.GetBool("Change"))
+            {
+                _oldStageId = _stageId;
+                _stageSelectImage.SetValue(stageNo);
+                _stageSelectAnim.SetBool("Select", true);
+            }
         }
         else
         {
+            // 島の選択が外れた場合
+            _oldStageId = _stageId;
             _stageSelectAnim.SetBool("Select", false);
         }
-    }
-
-    void SetNoChange()
-    {
-
     }
 }
