f0d98937022ec4f7c2ccc7a2093dbd362128c35a
こうだい <kohdai0831lynn@gmail.com>
2020-05-08 10:59:00 +0900

    書くとこ間違えていた

--- Assets/02_Game/Script/BoxEmission.cs
+++ Assets/02_Game/Script/BoxEmission.cs
@@ -6,15 +6,13 @@ public class BoxEmission : MonoBehaviour
 {
     [SerializeField]float _emissionSpeed = 0.03f;
     MeshRenderer mesh;
-    private AudioSource audioSource;
-    public AudioClip _SEBoxBreak;
-
+   
     // Start is called before the first frame update
     void Start()
     {
         mesh = GetComponent<MeshRenderer>();
         mesh.material.SetFloat("_Brightness", 0);
-        audioSource = GetComponent<AudioSource>();
+       
     }
 
     public void StartEmission()
@@ -26,7 +24,6 @@ public class BoxEmission : MonoBehaviour
         for (float i = 0.5f; i <= Mathf.PI; i += _emissionSpeed)
         {
             mesh.material.SetFloat("_Brightness", Mathf.Clamp(Mathf.Sin(i), 0, 1.0f));
-            audioSource.PlayOneShot(_SEBoxBreak);
             yield return null;
         }
     }
