c2ad1b207ab5367970d5713f9b28dbd32d898ae6
asuto <asutokazuna1110@gmail.com>
2020-05-18 16:36:18 +0900

    箱破壊の修正 レンズフレアの素材追加 ブランチに名前がなかったので作成

--- Assets/02_Game/Script/BaseObject.cs
+++ Assets/02_Game/Script/BaseObject.cs
@@ -328,9 +328,9 @@ public class BaseObject : MonoBehaviour
             {
                 _lifted = E_HANDS_ACTION.NONE;
                 WaitMode();
-                this.gameObject.SetActive(false);       //割れた箱を非表示に
-                Instantiate((GameObject)Resources.Load("Debris"), _nextPos, Quaternion.identity);       //箱の破片を生成
-                GameObject.FindGameObjectWithTag("Map").GetComponent<Map>()._gameOver = true;  // ゲームオーバーやで
+                BlockDestroy _blockDestroy = this.gameObject.AddComponent<BlockDestroy>();
+                _blockDestroy.destroyObject(_nextPos); //箱破壊の処理
+                GameObject.FindGameObjectWithTag("Map").GetComponent<Map>()._gameOver = true;
                 _gameOver = true;
             });
         }
