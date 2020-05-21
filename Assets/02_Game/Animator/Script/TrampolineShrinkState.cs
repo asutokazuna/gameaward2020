dd106fb5a1afeafd191dd5fc003109c28e3e612a
itorisa <chrysanthemum.zero211@gmail.com>
2020-05-21 19:00:56 +0900

    トランポリンのアニメーションを追加

---
+++ Assets/02_Game/Animator/Script/TrampolineShrinkState.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampolineShrinkState : StateMachineBehaviour
+{
+    [SerializeField] int _maxShrinkCount = 1;
+    float _speed;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _speed = animator.speed;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(animator.GetBool("Jump"))
+        {
+            if(animator.GetInteger("Count") >= _maxShrinkCount)
+            {
+                animator.SetBool("FinishTP", true);
+                animator.speed = _speed;
+            }
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (animator.GetInteger("Count") >= _maxShrinkCount)
+        {
+            animator.SetBool("FinishTP", false);
+            animator.SetBool("Jump", false);
+        }
+        animator.SetBool("StartJumpTP", false);
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
