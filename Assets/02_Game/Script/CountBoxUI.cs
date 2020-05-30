eb61702c2a340aa0ef5cc9f6bf8ac410b2c39df4
itorisa <chrysanthemum.zero211@gmail.com>
2020-05-30 23:28:40 +0900

    アイコンのゲーム中の満水になった箱の数に応じて変化するアニメーションの追加

--- Assets/02_Game/Script/CountBoxUI.cs
+++ Assets/02_Game/Script/CountBoxUI.cs
@@ -6,6 +6,7 @@
  * @date    2020/04/05(日)  画像表示に対応
  * @date    2020/05/05(火)  NumberImage.csの仕様変更に合わせて一部修正
  * @date    2020/05/09(土)  カラーの変更対応
+ * @date    2020/05/30(土)　アイコンのアニメーションを追加
  */
 
 using System.Collections;
@@ -26,10 +27,13 @@ public class CountBoxUI : MonoBehaviour
     public string _countBoxTag;
 
     // 画像表示用
-    public NumberImage          _tatalBoxImage;     //!< 箱全体数画像表示用
-    public FullBoxImage         _fullBoxImage;      //!< 箱全体数画像表示用
-    [SerializeField] Color[]    _countNumColor = default;     //!< 画像のカラー保持
-    StageNameUI                 _stageNameUI;       //!< 現在のステージ取得用
+    public NumberImage          _tatalBoxImage;                 //!< 箱全体数画像表示用
+    public FullBoxImage         _fullBoxImage;                  //!< 箱全体数画像表示用
+    [SerializeField] Color[]    _countNumColor = default;       //!< 画像のカラー保持
+    StageNameUI                 _stageNameUI;                   //!< 現在のステージ取得用
+    WaterAnim                   _waterAnim;                     //!< アイコンアニメーション
+    int                         _waterAnimCount;                //!< アイコンアニメーション管理用
+    float                       _waterAnimQuater;               //!< 1/4の個数を管理
 
     // Start is called before the first frame update
     void Start()
@@ -47,6 +51,10 @@ public class CountBoxUI : MonoBehaviour
         _tatalBoxImage.SetValue(_countTatalBox, false);
         _fullBoxImage.SetFullBoxColor(_countNumColor[_stageNameUI.PlanetID]);
         _fullBoxImage.AddFullBox(0);
+
+        _waterAnim = GameObject.Find("WaterBlockIconMask").GetComponent<WaterAnim>();
+        _waterAnimQuater = (float)_countTatalBox / 4;
+        _waterAnimCount = 0;
     }
 
     /**
@@ -57,7 +65,68 @@ public class CountBoxUI : MonoBehaviour
     */
     public void AddFullBox(int fullBoxNum)
     {
+        int _waterCount;
+
         _fullBoxImage.SetFullBoxColor(_countNumColor[_stageNameUI.PlanetID]);
         _fullBoxImage.AddFullBox(fullBoxNum);
+
+        if (fullBoxNum > 0)
+        {
+            _waterAnimCount++;
+            _waterCount = _waterAnimCount;
+
+            // 水のアニメーションセット
+            if (_waterCount >= _countTatalBox)              // すべての箱に水が入った時
+            {
+                _waterAnim.SetWaterAnim(5);
+            }
+            else if (_waterCount >= _waterAnimQuater * 3)   // 3/4以上の箱に水が入った時
+            {
+                _waterAnim.SetWaterAnim(4);
+            }
+            else if (_waterCount >= _waterAnimQuater * 2)   // 2/4以上の箱に水が入った時
+            {
+                _waterAnim.SetWaterAnim(3);
+            }
+            else if (_waterCount >= _waterAnimQuater)       // 1/4以上の箱に水が入った時
+            {
+                _waterAnim.SetWaterAnim(2);
+            }
+            else if (_waterCount == 1)                      // 1つの箱に水が入った時
+            {
+                _waterAnim.SetWaterAnim(1);
+            }
+            else if (_waterCount == 0)                      // 水の入った箱がひとつもないとき
+            {
+                _waterAnim.SetWaterAnim(0);
+            }
+        }
+        else
+        {
+            _waterCount = _waterAnimCount;
+            _waterAnimCount--;
+
+            // 水のアニメーションセット
+            if (_waterCount == 0)                               // 水の入った箱がひとつもないとき
+            {
+                _waterAnim.SetWaterAnim(0);
+            }
+            else if (_waterCount == 1)                          // 1つの箱に水が入った時
+            {
+                _waterAnim.SetWaterAnim(1);
+            }
+            else if (_waterCount >= _waterAnimQuater)           // 1/4以上の箱に水が入った時
+            {
+                _waterAnim.SetWaterAnim(2);
+            }
+            else if (_waterCount >= _waterAnimQuater * 2)       // 2/4以上の箱に水が入った時
+            {
+                _waterAnim.SetWaterAnim(3);
+            }
+            else if (_waterCount >= _waterAnimQuater * 3)       // 3/4以上の箱に水が入った時
+            {
+                _waterAnim.SetWaterAnim(4);
+            }
+        }
     }
 }
