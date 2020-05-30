333e9d03d64b8fc20c61b5808e7d5db27cc964c3
LightMikoshi <kurichan0408@gmail.com>
2020-05-30 15:03:23 +0900

    いったんマージ

---
+++ Assets/00_share/Script/CursorMan.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorMan : MonoBehaviour
+{
+    void Start()
+    {    
+        Cursor.visible = false; //マウスカーソル非表示
+    }
+}
