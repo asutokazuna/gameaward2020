b5eb04123d4a9b85b248c22576652bfe6c4cfabc
nakagami0522 <sweeeet.gami0522@gmail.com>
2020-06-01 22:03:06 +0900

    ゲームクリア時のキー設定 AnyTriggerの実装

--- Assets/01_Title/Script/Title.cs
+++ Assets/01_Title/Script/Title.cs
@@ -17,14 +17,7 @@ public class Title : MonoBehaviour
     {
         if (_LogoAnimFinish)
         {
-            for (int n = 0; n < (int)E_INPUT.MAX; n++)
-            {
-                if (GameObject.FindGameObjectWithTag("Input").GetComponent<Controller>().isInput(E_INPUT_MODE.TRIGGER, (E_INPUT)n))
-                {
-                    GameObject.FindGameObjectWithTag("SceneManager").GetComponent<SceneMgr>().SetScene(E_SCENE.STAGE_SELECT);
-                }
-            }
-            if (Input.anyKey)
+            if (GameObject.FindGameObjectWithTag("Input").GetComponent<Controller>().isAnyTrigger())
             {
                 GameObject.FindGameObjectWithTag("SceneManager").GetComponent<SceneMgr>().SetScene(E_SCENE.STAGE_SELECT);
             }
