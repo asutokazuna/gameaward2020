a9d9c34f006ba1aeade244fb447690fe91755ea9
itorisa <chrysanthemum.zero211@gmail.com>
2020-06-05 18:27:02 +0900

    クリアオブジェクトのアニメーションの仮システム

---
+++ Assets/04_Galaxy/script/ClearObjAnim.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearObjAnim : MonoBehaviour
+{
+    [SerializeField] Animator[]   _setObject;
+    [SerializeField] float          _startTime;
+    private int         _count;
+    private ClearObject _clearObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _clearObject = GameObject.Find("ClearObjectManager").GetComponent<ClearObject>();
+        _count = 0;
+
+        float time;
+
+        for (int i = 0; i < _setObject.Length; i++)
+        {
+            if (_clearObject._isChange)
+            {
+                time = _startTime + i * 0.1f;
+
+                Invoke("SetObject", time);
+            }
+            else
+            {
+                _setObject[i].SetBool("Finished", true);
+            }
+        }
+    }
+
+    void SetObject()
+    {
+        _setObject[_count].SetBool("Set", true);
+        _count++;
+    }
+}
--- Assets/04_Galaxy/script/ClearObject.cs
+++ Assets/04_Galaxy/script/ClearObject.cs
@@ -14,21 +14,50 @@ using UnityEngine;
  */
 public class ClearObject : MonoBehaviour
 {
-    [SerializeField] GameObject[] _gameObjects = default;   // オブジェクト管理
-    SceneMgr _sceneMgr;     // フラグ取得用
+    [SerializeField] GameObject[]   _gameObjects = default;     //!< オブジェクト管理
+    private SceneMgr                _sceneMgr;                  //!< フラグ取得用
+    static  private bool[]          _isClearFlg = new bool[60]; //!< クリア演出管理フラグ
+    static  private bool            _isInit = true;             //!< クリア演出管理初期化フラグ
+            public  bool            _isChange;                  //!< クリア演出管理フラグ
 
     // Start is called before the first frame update
     void Start()
     {
+        // 初期化
+        if(_isInit)
+        {
+            Init();
+        }
+
+        _isChange = false;
         _sceneMgr = GameObject.Find("SceneManager").GetComponent<SceneMgr>();
 
         for (int i = (int)E_SCENE._1_1; i < (int)E_SCENE.MAX; i++) 
         {
-            if(_gameObjects.Length - 1 < i - (int)E_SCENE._1_1)
+            if (_gameObjects.Length - 1 < i - (int)E_SCENE._1_1)
             {
                 break;
             }
-            _gameObjects[i - (int)E_SCENE._1_1].SetActive(_sceneMgr.GetStageClear((E_SCENE)i));
+            else if (_sceneMgr.GetStageClear((E_SCENE)i) && !_isClearFlg[i - (int)E_SCENE._1_1])
+            {
+                _isChange = true;
+                _isClearFlg[i - (int)E_SCENE._1_1] = true;
+                _gameObjects[i - (int)E_SCENE._1_1].SetActive(true);
+            }
+        }
+    }
+
+    void Init()
+    {
+        _isInit = false;
+
+        for (int i = 0; i < 60; i++)
+        {
+            if(_gameObjects.Length <= i)
+            {
+                break;
+            }
+            _gameObjects[i].SetActive(false);
         }
     }
 }
